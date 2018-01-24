71ed5d2252de246bf09ad179a0aa8bcc705aebbd
Prashi095 <35216055+Prashi095@users.noreply.github.com>
2018-01-24 08:55:36 -0800

    Create check.java

---
+++ check.java
@@ -0,0 +1,14 @@
+class new
+{
+public static void main(String args[])
+{
+Scanner sc = new Scanner(System.in);
+System.out.print("enter the no");
+int a=sc.nextInt();
+if(a==0)
+System.out.print("no is equal to 0");
+else if(a<0)
+System.out.print("no is negative");
+else 
+System.out.print("no is positive");
+}}
